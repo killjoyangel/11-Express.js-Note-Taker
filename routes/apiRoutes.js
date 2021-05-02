f953f66993e4144166d3ec3916f152b6a0c2f6cb
Ariel Marchand <marchand.aa@gmail.com>
2021-05-02 14:03:22 -0600

    final update

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,15 +1,14 @@
 
 const database = require('../db/db.json')
+const { v4: uuidv4 } = require("uuid");
 
 
 module.exports = (app) => {
 
     app.get('/api/notes', (req, res) => res.json(database));
-
-    app.post('/api/notes', (req, res) => {
-
-        database.push(req.body);
-        res.json(true);
-    })
-
-}
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body; 
+        newNote.id = uuidv4(); 
+        database.push(newNote); 
+        res.json(true); 
+    })};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const router = require('express').Router();
-//routing
+
 module.exports = (app) => {
   app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
